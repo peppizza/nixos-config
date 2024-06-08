9dad380690c6cc0327c2f732a1070cd8fa561bc0
Spencer Vess <spencervess@protonmail.com>
2024-06-08 09:27:16 -0400

    Fix stupidity

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -17,16 +17,6 @@
   home.file = {
   };
 
-  xdg.desktopEntries.osu-lazer = {
-    type = "Application";
-    name = "osu!lazer";
-    mimeType = [ "application/x-osu-skin-archive" "application/x-osu-replay" "application/x-osu-beatmap-archive" ];
-    icon = ./icon.png;
-    exec = "osu\\ !";
-    comment = "Open source free-to-win rhythm game";
-    categories = [ "Game" ];
-  };
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
--- modules/home-manager/osu/osu.nix
+++ modules/home-manager/osu/osu.nix
@@ -5,7 +5,7 @@
     type = "Application";
     name = "osu!lazer";
     mimeType = [ "application/x-osu-skin-archive" "application/x-osu-replay" "application/x-osu-beatmap-archive" ];
-    icon = lib.mkForce ./icon.png;
+    icon = ./icon.png;
     exec = "osu\\ !";
     comment = "Open source free-to-win rhythm game";
     categories = [ "Game" ];
