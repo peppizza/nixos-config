c8d440af06e18b74d2eab5a17151f842ff088f15
Spencer Vess <spencervess@protonmail.com>
2024-06-07 20:05:03 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }: {
+    nixosConfigurations = {
+      nixos-desktop = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+
+        specialArgs = { 
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+
+        modules = [./configuration.nix];
+      };
+    };
+  };
+}
