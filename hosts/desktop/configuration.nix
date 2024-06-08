fe1a945772b861774f18753b00009f9c2df27a59
Spencer Vess <spencervess@protonmail.com>
2024-06-07 23:42:03 -0400

    Set up swap to zram and fix suspend

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -153,8 +153,22 @@
     enableSSHSupport = true;
   };
 
+  services.zram-generator.enable = true;
+  services.zram-generator.settings = {
+    zram0 = {
+      zram-size = "ram / 2";
+      compression-algorithm = "zstd";
+    };
+  };
 
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 
+  systemd.tmpfiles.rules = [ "w /proc/acpi/wakeup - - - - XHC0" ];
 
   system.stateVersion = "24.05"; # DO NOT EDIT
 
