89e51cef004e31209162007a04c90437c9b270ca
Spencer Vess <spencervess@protonmail.com>
2024-06-10 23:58:59 -0400

    Please work

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -81,7 +81,7 @@
   services.fwupd.enable = true;
   services.auto-epp.enable = true;
 
-  boot.kernelParams = [ "amd_iommu=fullflush" "iommu=soft" ];
+  boot.kernelParams = [ "amd_iommu=off" "iommu=soft" ];
 
   system.stateVersion = "24.05"; # DO NOT CHANGE
 
