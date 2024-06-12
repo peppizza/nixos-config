c4c3c7a3e1aa73f860475307255feaffb01b0893
Spencer Vess <spencervess@protonmail.com>
2024-06-11 21:11:55 -0400

    It works!

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -41,7 +41,7 @@
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
-    # illuminanced
+    illuminanced
   ];
 
   fonts.packages = with pkgs; [
@@ -72,22 +72,19 @@
 
   services.fwupd.enable = true;
 
-  # services.syslogd.enable = true;
+  systemd.services.illuminanced = {
+    description = "Ambient light monitoring Service";
+    documentation = [ "https://github.com/mikhail-m1/illuminanced" ];
+    serviceConfig = {
+      Type = "forking";
+      ExecStart = "${pkgs.illuminanced}/bin/illuminanced -c /etc/illuminanced.toml";
+      PIDFile = "/run/illuminanced.pid";
+      Restart = "on-failure";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
-  # systemd.services.illuminanced = {
-  #   description = "Ambient light monitoring Service";
-  #   requires = [ "syslog.socket" ];
-  #   documentation = [ "https://github.com/mikhail-m1/illuminanced" ];
-  #   serviceConfig = {
-  #     Type = "forking";
-  #     ExecStart = "${pkgs.illuminanced} -c /etc/illuminanced.toml";
-  #     PIDFile = "/run/illuminanced.pid";
-  #     Restart = "on-failure";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  # };
-
-  # environment.etc."illuminanced.toml".source = ./illuminanced.toml;
+  environment.etc."illuminanced.toml".source = ./illuminanced.toml;
 
   system.stateVersion = "24.05"; # DO NOT CHANGE
 
