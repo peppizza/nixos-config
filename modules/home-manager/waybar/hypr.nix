e6713f745c3deddce3a72917df0cbd17435ab01c
Spencer Vess <spencervess@protonmail.com>
2024-06-13 15:10:57 -0400

    Fix waybar battery icons

--- modules/home-manager/waybar/hypr.nix
+++ modules/home-manager/waybar/hypr.nix
@@ -264,7 +264,7 @@
           format = "{icon}   {capacity}%";
           format-charging = "󰂄  {capacity}%";
           format-plugged = "  {capacity}%";
-          format-alt = "{icon}  {time}";
+          format-alt = "{icon}   {time}";
           format-icons = [ "" "" "" "" "" ];
         };
 
