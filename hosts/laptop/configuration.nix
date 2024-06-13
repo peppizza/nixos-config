peppizza/nixos-config

commit 03b013160a0885fd87aab0071a74eced0afecaa9

Add autoconnect for iwd

hosts/laptop/configuration.nix
@@ -22,8 +22,14 @@
     };
   };
 
-  networking.hostName = "nixos-laptop";
-  networking.networkmanager.wifi.backend = "iwd";
+  networking = {
+    hostName = "nixos-laptop";
+    networkmanager = {
+      wifi.backend = "iwd";
+      dhcp = "dhcpcd";
+    };
+    wireless.iwd.settings.Settings.AutoConnect = true;
+  };
 
   hardware.bluetooth = {
     enable = true;

commit 2a33e1d2bed8b505c50c074687f01eaf303eebe0

Remove dhcpcd fixes regression caused by 03b013160a0885fd87aab0071a74eced0afecaa9

hosts/laptop/configuration.nix
@@ -24,10 +24,7 @@
 
   networking = {
     hostName = "nixos-laptop";
-    networkmanager = {
-      wifi.backend = "iwd";
-      dhcp = "dhcpcd";
-    };
+    networkmanager.wifi.backend = "iwd";
     wireless.iwd.settings.Settings.AutoConnect = true;
   };
 