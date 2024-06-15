afd68d6780dddc632c2470031ae63f1a0abff350
Spencer Vess <spencervess@protonmail.com>
2024-06-15 10:05:06 -0400

    Break out hypridle and hyprlock into own modules

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -3,6 +3,8 @@
   imports = [
     ../waybar/hypr.nix
     ../tofi.nix
+    ./hyprlock.nix
+    ./hypridle.nix
   ];
 
   services = {
@@ -15,73 +17,6 @@
   services.mpd.enable = true;
   services.mpdris2.enable = true;
 
-  programs.hyprlock = {
-    enable = true;
-    settings= {
-      general = {
-        disable_loading_bar = true;
-        grace = 3;
-        hide_cursor = true;
-        no_fade_in = false;
-      };
-
-      background = [
-        {
-          path = "~/.config/wallpaper.png";
-          blur_passes = 3;
-          blur_size = 8;
-        }
-      ];
-
-      input-field = [
-        {
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = "'Password...'";
-          shadow_passes = 2;
-        }
-      ];
-    };
-  };
-
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-      };
-
-      listener = [
-        {
-          timeout = 150; # 2.5 minutes
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
-        }
-        {
-          timeout = 300; # 5 minutes
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 330; # 5.5 minutes
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpmms on";
-        }
-        {
-          timeout = 1800; # 30 minutes
-          on-timeout = "systemctl suspend";
-        }
-      ];
-    };
-  };
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
---
+++ modules/home-manager/hyprland/hypridle.nix
@@ -0,0 +1,33 @@
+{
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        {
+          timeout = 150; # 2.5 minutes
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = 300; # 5 minutes
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330; # 5.5 minutes
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpmms on";
+        }
+        {
+          timeout = 1800; # 30 minutes
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
