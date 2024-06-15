eb8e3ff53899ec28785b15261aae3b908970747a
Spencer Vess <spencervess@protonmail.com>
2024-06-15 10:17:40 -0400

    New hyprlock config

--- modules/home-manager/hyprland/hyprlock.nix
+++ modules/home-manager/hyprland/hyprlock.nix
@@ -1,35 +1,64 @@
 {
   programs.hyprlock = {
     enable = true;
-    settings= {
-      general = {
-        disable_loading_bar = true;
-        grace = 3;
-        hide_cursor = true;
-        no_fade_in = false;
+    settings = {
+      # BACKGROUND
+      background = {
+        path = "~/.config/wallpaper.png";
+        blur_passes = 3;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
       };
 
-      background = [
-        {
-          path = "~/.config/wallpaper.png";
-          blur_passes = 3;
-          blur_size = 8;
-        }
-      ];
+      # GENERAL
+      general = {
+        no_fade_in = false;
+        grace = 3;
+        disable_loading_bar = true;
+      };
 
-      input-field = [
+      # INPUT FIELD
+      input-field = {
+        size = "250, 60";
+        outline_thickness = 2;
+        dots_size = 0.2;
+        dots_spacing = 0.2;
+        dots_center = true;
+        outer_color = "rgba(0, 0, 0, 0)";
+        inner_color = "rgba(0, 0, 0, 0.5)";
+        font_color = "rgb(200, 200, 200)";
+        fade_on_empty = false;
+        font_family = "JetBrains Mono Nerd Font Mono";
+        placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
+        hide_input = false;
+        position = "0, -120";
+        halign = "center";
+        valign = "center";
+      };
+
+      label = [
+        # TIME
         {
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = "'Password...'";
-          shadow_passes = 2;
+          text = "cmd[update:1000] echo \"$(date +\"%-I:%M%p\")\"";
+          color = "$foreground";
+          font_size = 120;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -300";
+          halign = "center";
+          valign = "top";
+        }
+
+        # USER
+        {
+          text = "Hi there, $USER";
+          color = "$foreground";
+          font_size = 25;
+          font_family = "JetBrains Mono Nerd Font Mono";
+          position = "0, -40";
+          halign = "center";
+          valign = "center";
         }
       ];
     };
