ae9b97b8c063d91582a35795a6303b618e63d57b
Spencer Vess <spencervess@protonmail.com>
2024-07-14 10:36:15 -0400

    Add cuda cachix

--- modules/nixos/nixsettings.nix
+++ modules/nixos/nixsettings.nix
@@ -28,8 +28,8 @@
       ];
       warn-dirty = false;
 
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [ "https://hyprland.cachix.org" "https://cuda-maintainers.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
     };
 
     gc = {
