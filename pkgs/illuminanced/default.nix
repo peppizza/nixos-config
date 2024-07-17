91ae36a14a47488ea23c9ee6488521182fd6decf
Spencer Vess <spencervess@protonmail.com>
2024-07-17 16:00:25 -0400

    Clean up illuminanced pkg to prepare for upstreaming

--- pkgs/illuminanced/default.nix
+++ pkgs/illuminanced/default.nix
@@ -6,16 +6,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "illuminanced";
-  version = "0.1.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "peppizza";
-    repo = "illuminanced";
-    rev = "c5af3114b1d6241099b9424d41432372ef232d2e";
-    hash = "sha256-N6584PCIZ2rzVH2rAVv/WYPtUKbLEFhwVH3hbvcSSKo=";
+    repo = pname;
+    rev = version;
+    hash = "sha256-SohqLqAB0IVcHwurtbnVKi46YqU4/OuHK/CfWQgTHQM=";
   };
 
-  cargoHash = "sha256-wgNISV2jfZZDaOr3p+knd8qBAafckSOwjWmLOpaBiWQ=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  postPatch = ''
+    ln -sf ${./Cargo.toml} Cargo.toml
+    ln -sf ${./Cargo.lock} Cargo.lock
+  '';
 
   meta = with lib; {
     description = "A user mode daemon for automatically changing brightness based on light sensor value designed for modern laptops.";
