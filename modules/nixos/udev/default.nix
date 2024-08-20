af07dd4c8823a5b5143c2380ad179d76f4b3edc7
Spencer Vess <spencervess@protonmail.com>
2024-08-19 20:47:29 -0400

    Fix probe-rs udev rule priority
    Formatting

--- modules/nixos/udev/default.nix
+++ modules/nixos/udev/default.nix
@@ -1,1 +1,10 @@
-{ services.udev.extraRules = builtins.readFile (./69-probe-rs.rules); }
+{ lib, pkgs, ... }:
+{
+  services.udev.packages = lib.singleton (
+    pkgs.writeTextFile {
+      name = "probe-rs-rules";
+      text = builtins.readFile (./69-probe-rs.rules);
+      destination = "/etc/udev/rules.d/69-probe-rs.rules";
+    }
+  );
+}
