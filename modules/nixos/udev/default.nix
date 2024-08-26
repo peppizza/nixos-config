dd769ad07a599ce5ff491341a3db8831d78a9f30
Spencer Vess <spencervess@protonmail.com>
2024-08-25 22:01:05 -0400

    Fix wooting udev rules

--- modules/nixos/udev/default.nix
+++ modules/nixos/udev/default.nix
@@ -1,10 +1,13 @@
 { lib, pkgs, ... }:
 {
-  services.udev.packages = lib.singleton (
-    pkgs.writeTextFile {
-      name = "probe-rs-rules";
-      text = builtins.readFile (./69-probe-rs.rules);
-      destination = "/etc/udev/rules.d/69-probe-rs.rules";
-    }
-  );
+  services.udev = {
+    packages = lib.singleton (
+      pkgs.writeTextFile {
+        name = "probe-rs-rules";
+        text = builtins.readFile (./69-probe-rs.rules);
+        destination = "/etc/udev/rules.d/69-probe-rs.rules";
+      }
+    );
+    extraRules = builtins.readFile (./70-wooting.rules);
+  };
 }
